e60ff4a44ada7b210a4c7790e4582a965862bbbe
Vaibhav Bhapkar <vaibhavbhapkar62@gmail.com>
2022-02-21 21:50:21 +0530

    product section done

--- projectbackend/controllers/product.js
+++ projectbackend/controllers/product.js
@@ -2,8 +2,9 @@ const Product = require('../models/product');
 const formidable = require("formidable");
 const _ = require("lodash");
 const fs = require("fs");
+const { parseInt } = require('lodash');
 
-
+//Param
 exports.getProductById = (req, res, next, id)=>{
     Product.findById(id)
     .populate("category")
@@ -18,7 +19,9 @@ exports.getProductById = (req, res, next, id)=>{
     })
 }
 
+//CONTROLLERS
 
+//create controllers
 exports.createProduct= (req, res)=>{
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
@@ -70,3 +73,126 @@ exports.createProduct= (req, res)=>{
         })
     })
 };
+
+exports.getProduct = (req, res)=>{
+    req.product.photo = undefined;
+    return res.json(req.product);
+}
+
+//delete controllers
+exports.deleteProduct = (req, res)=>{
+    let product = req.product;
+    product.remove((err, deletedProduct)=>{
+        if(err){
+            return res.status(400).json({
+                error: "Failed to delete the product"
+            })
+        }
+        res.json({
+            message: "Deletion was a success",
+            deletedProduct
+        })
+    })
+}
+
+//update controllers
+exports.updateProduct = (req, res)=>{
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, file)=>{
+        if(err){
+            return res.status(400).json({
+                error: "problem with img"
+            });
+        }
+
+        //update 
+        let product = req.product;
+        product = _.extend(product, fields)
+
+        //handle file here 
+        if(file.photo){
+            if(file.photo.size > 3000000){
+                return res.status(400).json({
+                    error: "file size is to big"
+                })
+            } 
+            product.photo.data = fs.readFileSync(file.photo.path)
+            product.photo.contentType = file.photo.type
+        }
+
+
+        //Save to the DB
+        product.save((err, product)=>{
+            if(err){
+                return res.status(400).json({
+                    error: "Fail to update the img"
+                });
+            }
+            res.json(product)
+        })
+    })
+}
+
+exports.getAllProducts = (req, res)=>{
+    let limit= req.query.limit? parseInt(req.query.limit) : 8;
+    let sortBy= req.query.sortBy? parseInt(req.query.sortBy) : "_id";
+
+
+    Product.find()
+    .select("-photo")
+    .populate("Category")
+    .sort([[sortBy, "asc"]])
+    .limit(limit)
+    .exec((err, products)=>{
+        if(err){
+            return res.status(400).json({
+                error: "No product Found"
+            })
+        }
+        res.json(products);
+    })
+}
+
+exports.getAllUniqueCategories = (req, res)=>{
+    Product.distinct("category", {},  (err, category)=>{
+        if(err){
+            return res.status(400).json({
+                error: "No category found"
+            })
+        }
+        res.json(category);
+    })
+}
+
+// MIDDLEWARES
+exports.photo = (req, res, next)=>{
+    if(req.product.photo.data){
+        res.set("Content-Type", req.product.photo.contentType);
+        return res.send(req.product.photo.data);
+    }
+    next();
+}
+
+exports.updateStock = (req, res, next)=>{
+    let myOperations = req.body.order.products.map(prod=>{
+        return{
+            updateOne: {
+                filter: {_d: prod._id},
+                update: {$inc: {stock: -prod.count, sold: +prod.count}}
+            }
+        }
+    })
+    Product.bulkWrite(myOperations, {}, (err, products)=>{
+        if(err){
+            return res.status(400).json({
+                error:"bulk opration fail"
+            })
+        }
+
+        next();
+    })
+    next()
+}
+
+
--- projectbackend/routes/product.js
+++ projectbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {getProductById, createProduct} = require("../controllers/product");
+const {getProductById, createProduct, getProduct, photo, deleteProduct, updateProduct, getAllProducts, getAllUniqueCategories} = require("../controllers/product");
 const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth");
 const {getUserById} = require("../controllers/user");
 
@@ -11,8 +11,23 @@ router.param("productId", getProductById);
 
 
 // all of actual routes
-router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct)
+//CREATE
+router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct);
+
+//READ
+router.get("/product/:productId", getProduct);
+router.get("/product/photo/:productId", photo);
+
+//DELETE
+router.delete("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, deleteProduct);
+router.put("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, updateProduct);
+
+//UPDATE
+
+//LISTING ROUTE
+router.get("/products", getAllProducts)
 
 
+router.get("/products/categories", getAllUniqueCategories);
 
 module.exports = router;
