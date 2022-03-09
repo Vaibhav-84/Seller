4bc7ca2f6b0661112c37967493c1fce836a01c97
Vaibhav Bhapkar <vaibhavbhapkar62@gmail.com>
2022-03-10 01:17:33 +0530

    frontend connection

---
+++ projfrontend/src/core/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../styles.css"
+
+
+export default function Home(){
+    return (
+        <div>
+            <h1 className='text-white'>Home Page</h1>
+        </div>
+    );
+}
